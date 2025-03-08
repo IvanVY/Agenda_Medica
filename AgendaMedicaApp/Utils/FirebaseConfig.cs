487e1063c5ae4c5bf5f478d6c102d3ff0851c296
Ivan Vera <ivandejesusvera@gmail.com>
2025-03-08 00:32:54 -0500

    Subiendo proyecto inicial de Agenda Médica

---
+++ AgendaMedicaApp/Utils/FirebaseConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaMedicaApp.Utils
+{
+    public static class FirebaseConfig
+    {
+        public static string CredentialsPath => @"C:\Users\NewPc\source\repos\AgendaMedicaApp\AgendaMedicaApp\firebase-adminsdk.json"; // Cambia esta ruta
+        public static string ProjectId => "agendadecitas-fe25e";
+    }
+}
